e4a5357f4c79044c334507414ea5f96878d95bfa
pritexx <vladon_2004@mail.ru>
2023-05-05 21:59:44 +0300

    added xml docs support for swagger

--- VkTechTest/Program.cs
+++ VkTechTest/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(s =>
+{
+    s.SwaggerDoc("v1", new OpenApiInfo{Title = "User service", Version = "v1"});
+    s.IncludeXmlComments("bin/Debug/VkTechTest.xml");
+});
 
 var app = builder.Build();
 
