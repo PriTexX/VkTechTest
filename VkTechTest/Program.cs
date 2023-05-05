f35afe8d419a91953e9bbe221c06a488ec43d983
pritexx <vladon_2004@mail.ru>
2023-05-05 22:18:46 +0300

    added loading env vars from .env file

--- VkTechTest/Program.cs
+++ VkTechTest/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using VkTechTest.Database;
 
+LoadEnvVariablesFromFile(".env");
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationContext>(c => 
@@ -31,3 +33,21 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void LoadEnvVariablesFromFile(string filePath)
+{
+    if (!File.Exists(filePath))
+        return;
+
+    foreach (var line in File.ReadAllLines(filePath))
+    {
+        var parts = line.Split(
+            '=',
+            StringSplitOptions.RemoveEmptyEntries);
+
+        if (parts.Length != 2)
+            continue;
+
+        Environment.SetEnvironmentVariable(parts[0], parts[1]);
+    }
+}
