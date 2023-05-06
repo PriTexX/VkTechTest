27d556b9d6b01b5eb8a9090536e5a78c62fe1f6f
pritexx <vladon_2004@mail.ru>
2023-05-06 17:50:01 +0300

    implemented SHA256 password hasher

---
+++ VkTechTest/Services/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace VkTechTest.Services.Interfaces;
+
+internal interface IPasswordHasher
+{
+    public string GetHash(string password);
+
+    public bool Verify(string hash, string password);
+}
