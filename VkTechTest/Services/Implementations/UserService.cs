3feee0d839f16ee912d350cb40baf932e1460c66
pritexx <vladon_2004@mail.ru>
2023-05-06 17:53:46 +0300

    added interface for UserService

---
+++ VkTechTest/Services/Implementations/UserService.cs
@@ -0,0 +1,24 @@
+﻿using VkTechTest.Models.Enums;
+using VkTechTest.Services.Interfaces;
+
+namespace VkTechTest.Services.Implementations;
+
+public sealed class UserService : IUserService
+{
+    private readonly IPasswordHasher _passwordHasher;
+
+    public UserService(IPasswordHasher passwordHasher)
+    {
+        _passwordHasher = passwordHasher;
+    }
+
+    public Task<int> Register(string login, string password, UserGroupType userGroupType, UserStateType userStateType)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> Login(string login, string password)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ VkTechTest/Services/Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using VkTechTest.Models.Enums;
+
+namespace VkTechTest.Services.Interfaces;
+
+public interface IUserService
+{
+    public Task<int> Register(string login, string password, UserGroupType userGroupType, UserStateType userStateType);
+
+    public Task<int> Login(string login, string password);
+}
