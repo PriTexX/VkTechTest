6f7d72cebf871797c3aa11a612bf71a4c4b63966
pritexx <vladon_2004@mail.ru>
2023-05-07 13:54:50 +0300

    added UserAlreadyExistsException

---
+++ VkTechTest/Models/Exceptions/UserAlreadyExistsException.cs
@@ -0,0 +1,7 @@
+﻿namespace VkTechTest.Models.Exceptions;
+
+public class UserAlreadyExistsException : Exception
+{
+    public UserAlreadyExistsException(long userId) : base($"User with id: {userId} already exists")
+    {}
+}
