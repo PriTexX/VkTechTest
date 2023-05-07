6f54f1d2a288ac839bdd1e2bddfb94890a68ad97
pritexx <vladon_2004@mail.ru>
2023-05-07 13:54:39 +0300

    added UserRepository

---
+++ VkTechTest/Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VkTechTest.Database;
+using VkTechTest.Database.Models;
+using VkTechTest.Models.Exceptions;
+using VkTechTest.Repositories.Interfaces;
+
+namespace VkTechTest.Repositories.Implementations;
+
+public sealed class UserRepository : IUserRepository
+{
+    private readonly ApplicationContext _applicationContext;
+
+    public UserRepository(ApplicationContext applicationContext)
+    {
+        _applicationContext = applicationContext;
+    }
+
+    public async Task<UserEntity> SaveUserAsync(UserEntity userEntity)
+    {
+        var userExists = await _applicationContext.Users.AnyAsync(u => u.Login == userEntity.Login);
+
+        if (userExists)
+        {
+            throw new UserAlreadyExistsException(userEntity.Id);
+        }
+
+        _applicationContext.Users.Add(userEntity);
+        await _applicationContext.SaveChangesAsync();
+
+        return userEntity;
+    }
+}
---
+++ VkTechTest/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using VkTechTest.Database.Models;
+
+namespace VkTechTest.Repositories.Interfaces;
+
+public interface IUserRepository
+{
+    public Task<UserEntity> SaveUserAsync(UserEntity userEntity);
+}
